50e1588acf6fb556b6c42e53440fd13bb97960ab
Kehinde Ojewale <kennyoje1@gmail.com>
2022-07-06 01:53:18 +0100

    ...

--- Enum/AccountStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace BankingConsoleApp.Enum
-{
-    public enum AccountStatus
-    {
-        Active = 1,
-        Disabled = 2
-    }
-}
--- Enum/TransactionType.cs
+++
@@ -1,8 +0,0 @@
-namespace BankingConsoleApp.Enum
-{
-    public enum TransactionType
-    {
-        Debit = 1,
-        Credit = 2
-    }
-}
